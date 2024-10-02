1cecbf1b3afbc216ec50b2bfe1fdb5b9fb935818
신동근 <donggeun.shin@mirero.co.kr>
2024-10-02 10:22:36 +0900

    [FE] chore: 주석 변경

--- React-Project/FrontEnd/src/App.tsx
+++ React-Project/FrontEnd/src/App.tsx
@@ -3,7 +3,7 @@ import TodoPage from './pages/TodoPage';
 function App() {
   return (
     <div className="App">
-      {/* TodoContainer를 호출하여 Todo 기능을 화면에 렌더링 */}
+      {/* TodoPage를 호출하여 Todo 기능을 화면에 렌더링 */}
       <TodoPage />
     </div>
   );

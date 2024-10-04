9494bde94560573ff2e6b06cae922e9dc31eb42d
신동근 <donggeun.shin@mirero.co.kr>
2024-10-04 10:10:42 +0900

    [FE] refactor: TodoItem 수정 메서드 form태그를 이용하는것으로 리팩토링 작업

--- React-Project/FrontEnd/src/components/Todo/TodoItem.tsx
+++ React-Project/FrontEnd/src/components/Todo/TodoItem.tsx
@@ -14,22 +14,26 @@ export default function TodoItem({ task, completed, id }: TodoItemProps) {
   const [newTask, setNewTask] = useState(task); // 수정 중인 할 일 내용을 로컬 상태로 관리
 
   // 수정 내용 저장 메서드
-  const handleSave = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
     updateTodo(id, newTask); // zustand에서 할 일 내용 업데이트
     setIsUpdating(false); // 수정 모드 종료
-  };
+  }
 
   return (
     <div className="bg-blue-50 flex items-center justify-between">
       <div>
         <input type="checkbox" checked={completed} onChange={() => toggleTodo(id)} />
+        
+        {/* form 태그로 수정 모드를 감싸서 submit 이벤트 처리*/}
         {isUpdating ? (
-          <input
-            className="ml-2 border p-1"
-            value={newTask} // 수정 중인 내용 바인딩
-            onChange={(e) => setNewTask(e.target.value)} // 입력값 변경 시 상태 업데이트
-            onKeyDown={(e) => e.key === 'Enter' && handleSave()} // Enter키로 저장
-          />
+          <form onSubmit={handleSubmit}>
+            <input
+              className="ml-2 border p-1"
+              value={newTask} // 수정 중인 내용 바인딩
+              onChange={(e) => setNewTask(e.target.value)} // 입력값 변경 시 상태 업데이트
+            />
+          </form>
         ) : (
           <span className={completed ? 'line-through ml-2' : 'ml-2'}>
             {task} {/* 완료된 경우 텍스트에 줄 긋기 적용 */}
@@ -40,8 +44,8 @@ export default function TodoItem({ task, completed, id }: TodoItemProps) {
       <div>
         {/* 저장 및 수정 관련 버튼 부분*/}
         {isUpdating ? (
-          // 저장 버튼: MUI의 Save 아이콘 버튼
-          <IconButton color="primary" onClick={handleSave}>
+          // 저장 버튼: MUI의 Save 아이콘 버튼, type="submit"을 통해 form의 submit을 트리거
+          <IconButton color="primary" type="submit" form="edit-form">
             <SaveIcon />
           </IconButton>
         ) : (

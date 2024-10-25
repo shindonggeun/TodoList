06b23020e13588208257a8c1dcca95999f1f1818
신동근 <donggeun.shin@mirero.co.kr>
2024-10-25 10:49:50 +0900

    [FE] feat: React Query Devtools 설치 및 적용

--- React-Project/FrontEnd/src/main.tsx
+++ React-Project/FrontEnd/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App.tsx'
 import './index.css'
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 )
